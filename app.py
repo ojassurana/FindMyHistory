7c96e5f0dbcf0ca1606be3efd08dfe57e2b42bcd
ojassurana <me@ojassurana.com>
2026-04-22 16:41:17 +0800

    Fix TemplateResponse API for newer Starlette

--- app.py
+++ app.py
@@ -212,7 +212,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
 
 
 @app.get("/api/status")
